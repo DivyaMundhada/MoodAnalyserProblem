2c47b59ebece2ec8d93ac0ab47de86a0ddb9b64f
DivyaMundhada <divyamundada9@gmail.com>
2021-07-16 10:09:24 +0530

    Added Welcome Message.

---
+++ src/main/java/com/bridgelabz/moodanalyserprogram/MoodAnalyser.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.moodanalyserprogram;
+
+public class MoodAnalyser {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome To Mood Analyser Program.");
+
+	}
+
+}
